15bf5c6d541b9f269a9730b66b5e23096fe242d4
lestersinjacreater <lestetsinjawatosia@gmail.com>
2025-02-07 19:59:50 +0300

    setting up shit

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://upward-sloth-33.clerk.accounts.dev/",
+      domain: "https://inspired-donkey-77.clerk.accounts.dev/",
       applicationID: "convex",
     },
   ],
