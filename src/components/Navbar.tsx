c7bb33452a260da98da1bdc29f973555a47c8a29
daniel-kav <dankavatha01@gmail.com>
2025-02-07 17:29:38 +0300

    ModeToggle component and integrate it into Navbar for theme switching functionality

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link"
+import { ModeToggle } from "./ModeToggle"
+import { SignedIn, UserButton } from "@clerk/nextjs"
+import DasboardBtn from "./DasboardBtn"
+
+function Navbar() {
+  return (
+    <nav className="border-b border-gray-800 bg-gray-900">
+      <div className="flex h-16 items-center px-4 container mx-auto">
+        {/* LEFT SIDE -LOGO */}
+        <Link
+          href="/"
+          className="flex items-center gap-2 font-semibold text-2xl mr-6 font-mono hover:opacity-80 transition-opacity"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-8 h-8 text-blue-500"
+          >
+            <path
+              strokeLinecap="round"
+              d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
+            />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
+          </svg>
+          <span className="bg-gradient-to-r from-blue-500 to-blue-300 bg-clip-text text-transparent animate-gradient">
+            HireVueX
+          </span>
+        </Link>
+
+        {/* RIGHT SIDE - ACTIONS */}
+        <SignedIn>
+          <div className="flex items-center space-x-4 ml-auto">
+            <DasboardBtn />
+            <ModeToggle />
+            <UserButton />
+          </div>
+        </SignedIn>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
+
